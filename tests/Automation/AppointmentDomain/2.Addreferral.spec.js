3a923ccbe5efa5c3f46e589c0ddab8839ddf85c2
manoj.vyavahare <manoj.vyavahare@riomed.com>
2025-06-07 11:35:23 +0530

    User excel update

--- tests/Automation/AppointmentDomain/2.Addreferral.spec.js
+++ tests/Automation/AppointmentDomain/2.Addreferral.spec.js
@@ -24,34 +24,20 @@ import TopBlueBar from "../../../Pages/BaseClasses/TopBlueBar";
 import EditPatient from "../../../Pages/PatientDomain/EditPatient";
 import AddReferral from "../../../Pages/PatientDomain/AddReferral";
 
-const logindata = JSON.parse(
-  JSON.stringify(require("../../../TestData/PatientDomain/Login.json"))
+const logindata = JSON.parse(JSON.stringify(require("../../../TestData/PatientDomain/Login.json")));
+const patientdetailsdata = JSON.parse(JSON.stringify(require("../../../TestData/AppointmentDomain/PatientDetails.json"))
 );
-const patientdetailsdata = JSON.parse(
-  JSON.stringify(
-    require("../../../TestData/AppointmentDomain/PatientDetails.json")
-  )
-);
-const pipdetailsdata = JSON.parse(
-  JSON.stringify(require("../../../TestData/PatientDomain/PIPDetails.json"))
-);
-const gpdata = JSON.parse(
-  JSON.stringify(require("../../../TestData/PatientDomain/NewGPDetails.json"))
-);
+const pipdetailsdata = JSON.parse(JSON.stringify(require("../../../TestData/PatientDomain/PIPDetails.json")));
+const gpdata = JSON.parse(JSON.stringify(require("../../../TestData/PatientDomain/NewGPDetails.json")));
 
 const consoleLogs = [];
 let jsonData;
 
 test.describe("Appointment Domain Db COmparison", () => {
   test("Extract Patient Details", async ({}) => {
-    const excelFilePath =
-      process.env.EXCEL_FILE_PATH || "./ExcelFiles/AppointmentDomain.xlsx";
-    const jsonFilePath =
-      "./TestDataWithJSON/AppointmentDomain/AppointmentDetails.json";
-    const conversionSuccess = await convertExcelToJson(
-      excelFilePath,
-      jsonFilePath
-    );
+    const excelFilePath =process.env.EXCEL_FILE_PATH || "./ExcelFiles/AppointmentDomain.xlsx";
+    const jsonFilePath ="./TestDataWithJSON/AppointmentDomain/AppointmentDetails.json";
+    const conversionSuccess = await convertExcelToJson(excelFilePath,jsonFilePath);
 
     if (conversionSuccess) {
       jsonData = require("../../../TestDataWithJSON/AppointmentDomain/AppointmentDetails.json");
