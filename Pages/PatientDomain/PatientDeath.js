ab24f5780e3a93a337af2996793322f6531e268c
manoj.vyavahare <manoj.vyavahare@riomed.com>
2025-06-19 22:13:45 +0530

    Patientdeath.js change

--- Pages/PatientDomain/PatientDeath.js
+++ Pages/PatientDomain/PatientDeath.js
@@ -15,7 +15,7 @@ class PatientDeath
         this.calendar_DateofDeath=page.getByTestId('Date of Death')
         
         //this.dropdown_MarkPatientAsDead=page.getByRole('button', { name: 'No' })
-        this.dropdown_MarkPatientAsDead=page.getByTestId('Mark the Patient Dead').getByLabel('No')
+        this.dropdown_MarkPatientAsDead=page.locator("xpath=//div[@data-testid='Discharge and remove pending referrals from all services']")
         this.checkbox_SearchAntecedentcauseofDeath=page.getByTestId('Search Antecedent cause of Death').getByLabel('Search Antecedent cause of Death')
 
         this.btn_Ok=page.getByTestId('Ok')
