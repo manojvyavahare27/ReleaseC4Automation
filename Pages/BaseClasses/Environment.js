07b7849605438232f919827c34d598b8e7d4862c
manoj.vyavahare <manoj.vyavahare@riomed.com>
2025-07-12 14:50:33 +0530

    Aligned Environment

--- Pages/BaseClasses/Environment.js
+++ Pages/BaseClasses/Environment.js
@@ -1,14 +1,13 @@
-export default class Environment{
-    constructor(page)
-    {
-        this.page=page
-        //this.Test="http://localhost:3000/cellmaUser/login";
-      // this.Test="http://10.0.0.64:3000/cellmaUser/login"
-      this.Test="https://testcellma.com/cellmaUser/login"
-        //this.Test="http://10.0.0.63:3000/cellmaUser/login";
-        //this.Test="https://10.0.0.63/cellmaUser/login"
-       // this.Test1="https://10.0.0.99/cellmaUserAPI/swagger-ui/index.html"
-       // this.Test="https://10.0.0.63/cellmaUser/login"
-       //this.RefPortal="http://10.0.0.106:8080/cellmaPortal/portal/login"
-    }     
+export default class Environment {
+  constructor(page) {
+    this.page = page
+    //this.Test="http://localhost:3000/cellmaUser/login";
+    // this.Test="http://10.0.0.64:3000/cellmaUser/login"
+    this.Test = "https://testcellma.com/cellmaUser/login"
+    //this.Test="http://10.0.0.63:3000/cellmaUser/login";
+    //this.Test="https://10.0.0.63/cellmaUser/login"
+    // this.Test1="https://10.0.0.99/cellmaUserAPI/swagger-ui/index.html"
+    // this.Test="https://10.0.0.63/cellmaUser/login"
+    //this.RefPortal="http://10.0.0.106:8080/cellmaPortal/portal/login"
+  }
 }
